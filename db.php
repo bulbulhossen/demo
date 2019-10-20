001c47a6fc49a946aff845747517008ef8de1870
Md Bulbul Hossen <12374747+bulbulhossenbd@users.noreply.github.com>
2019-10-20 22:42:09 +0600

    Update and rename db.json to db.php

--- db.php
+++ db.php
@@ -3,18 +3,18 @@
         "id": "1",
         "title": "Slide 1",
         "description": "First of three slide",
-        "image_url": "http:\/\/localhost\/sliderimg\/slide1.png"
+        "image_url": "https://i.ibb.co/g4QQNdt/banner.jpg"
     },
     {
         "id": "2",
         "title": "Slide 2",
         "description": "Second of three slide",
-        "image_url": "http:\/\/localhost\/sliderimg\/slide2.png"
+        "image_url": "https://i.ibb.co/g4QQNdt/banner.jpg"
     },
     {
         "id": "3",
         "title": "Slide 3",
         "description": "Third of three slide",
-        "image_url": "http:\/\/localhost\/sliderimg\/slide3.png"
+        "image_url": "https://i.ibb.co/g4QQNdt/banner.jpg"
     }
 ]
