e474d983b7040dfc7246dc11ccc181aa1930a812
Md Bulbul Hossen <12374747+bulbulhossenbd@users.noreply.github.com>
2019-11-10 19:27:40 +0600

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
         "id": "1",
         "title": "Slide 1",
         "description": "First of three slide",
-        "image_url": "https://i.ibb.co/g4QQNdt/banner.jpg"
+        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
     },
     {
         "id": "2",
@@ -15,18 +15,18 @@
         "id": "3",
         "title": "Slide 3",
         "description": "Third of three slide",
-        "image_url": "https://i.ibb.co/g4QQNdt/banner.jpg"
+        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
     },
     {
         "id": "4",
         "title": "Slide 3",
         "description": "Third of three slide",
-        "image_url": "https://i.ibb.co/g4QQNdt/banner.jpg"
+        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
     },
     {
         "id": "5",
         "title": "Slide 3",
         "description": "Third of three slide",
-        "image_url": "https://i.ibb.co/g4QQNdt/banner.jpg"
+        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
     }
 ]
