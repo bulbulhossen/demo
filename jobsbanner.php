fc12929969b091e47144acfb9b8e27a6faa3ef0e
Md Bulbul Hossen <12374747+bulbulhossenbd@users.noreply.github.com>
2019-11-13 22:10:19 +0600

    Create jobsbanner.php

---
+++ jobsbanner.php
@@ -0,0 +1,32 @@
+[
+    {
+        "id": "1",
+        "title": "Slide 1",
+        "description": "First of three slide",
+        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
+    },
+    {
+        "id": "4",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
+    },
+    {
+        "id": "5",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
+    }
+]
