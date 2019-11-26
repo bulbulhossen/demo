705cd1e266282a249ca50900005c563db6abb025
Md Bulbul Hossen <12374747+bulbulhossenbd@users.noreply.github.com>
2019-11-26 22:44:13 +0600

    Update jobsbanner.php

--- jobsbanner.php
+++ jobsbanner.php
@@ -3,30 +3,30 @@
         "id": "1",
         "title": "Slide 1",
         "description": "First of three slide",
-        "image_url": "https://i.ibb.co/5xVCs8P/bannercareernewsapps.png"
+        "image_url": "https://i.ibb.co/yF9nhpv/jobsbannermain.png"
     },
     {
         "id": "2",
         "title": "Slide 2",
         "description": "Second of three slide",
-        "image_url": "https://i.ibb.co/5xVCs8P/bannercareernewsapps.png"
+        "image_url": "https://i.ibb.co/yF9nhpv/jobsbannermain.png"
     },
     {
         "id": "3",
         "title": "Slide 3",
         "description": "Third of three slide",
-        "image_url": "https://i.ibb.co/5xVCs8P/bannercareernewsapps.png"
+        "image_url": "https://i.ibb.co/yF9nhpv/jobsbannermain.png"
     },
     {
         "id": "4",
         "title": "Slide 3",
         "description": "Third of three slide",
-        "image_url": "https://i.ibb.co/5xVCs8P/bannercareernewsapps.png"
+        "image_url": "https://i.ibb.co/yF9nhpv/jobsbannermain.png"
     },
     {
         "id": "5",
         "title": "Slide 3",
         "description": "Third of three slide",
-        "image_url": "https://i.ibb.co/5xVCs8P/bannercareernewsapps.png"
+        "image_url": "https://i.ibb.co/yF9nhpv/jobsbannermain.png"
     }
 ]
