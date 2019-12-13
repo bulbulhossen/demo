f6df0487b072a1466ca50e9661c9e2001324f378
Md Bulbul Hossen <12374747+bulbulhossenbd@users.noreply.github.com>
2019-12-13 15:52:52 +0600

    Update deshonlineshop.php

--- deshonlineshop.php
+++ deshonlineshop.php
@@ -1,27 +1,27 @@
 [
     {
         "id": "1",
-        "title": "Slide 1",
-        "description": "First of three slide",
-        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
+        "title": "BPL",
+        "description": "Live Score",
+        "image_url": "https://i.ibb.co/tM0Q6nJ/bplbanners.png"
     },
     {
         "id": "2",
-        "title": "Slide 2",
-        "description": "Second of three slide",
-        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
+        "title": "News",
+        "description": "News Apps",
+        "image_url": "https://i.ibb.co/YcpbJF9/newsbannerss.png"
     },
     {
         "id": "3",
         "title": "Slide 3",
-        "description": "Third of three slide",
-        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
+        "description": "Jobs",
+        "image_url": "https://i.ibb.co/RPKBr6N/jobsnewsbanners.png"
     },
     {
         "id": "4",
         "title": "Slide 3",
-        "description": "Third of three slide",
-        "image_url": "https://i.ibb.co/2qHDkRH/welcome.jpg"
+        "description": "Results",
+        "image_url": "https://i.ibb.co/HPt7swS/jscresultsbanners.pngg"
     },
     {
         "id": "5",
